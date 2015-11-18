15a37703318e5dbcf13de9989b4dac1b4ea9aa3c
Jongyoul Lee <jongyoullee1@gmail.com>
2015-11-18 10:41:07 -0500

    added add and subtract functions

--- Rational.java
+++ Rational.java
@@ -2,22 +2,52 @@ public class Rational {
     private int numerator;
     private int denominator;
     public Rational() {
-	numerator = 0;
-	denominator = 1;
+	    numerator = 0;
+	    denominator = 1;
     }
-    public Rational(int numer, int denomer) {
-	this();
-	if (denomer == 0) {
-	    System.out.println("ERROR! Rational created with zero denominator");
-	} else {
-	    numerator = numer;
-	    denominator = denomer;
-	}
+    public Rational(int numer, int denom) {
+	    this();
+	    if (denom == 0) {
+	        System.out.println("ERROR! Rational created with zero denominator");
+	    } else {
+	        numerator = numer;
+	        denominator = denom;
+    	}
     }
     public String toString() {
-	return numerator + "/" + denominator;
+    	return numerator + "/" + denominator;
     }
     public double floatValue() {
-	return (double)numerator / (double)denominator;
+    	return (double)numerator / denominator;
+    }
+    public void multiply (Rational s) {
+        this.numerator=this.numerator*s.numerator;
+        this.denominator=this.denominator*s.denominator;
+    }
+    public void divide (Rational s) {
+        Rational a = new Rational(s.denominator,s.numerator);
+        this.multiply(a);
+    }
+    public void add(Rational s) {
+	this.numerator=this.numerator * s.denominator + this.denominator * s.numerator;
+	this.denominator = this.denominator * s.denominator;
+    }
+    public void subtract(Rational s) {
+	new Rational r = new Rational (-s.numerator,s.denominator);
+	this.add(r);
+    }
+    public static void main(String[] args) {
+        Rational r = new Rational(2,3); 
+        Rational s = new Rational(1,2); 
+        r.multiply(s);
+        System.out.println(r);
+        Rational t = new Rational(2,3); 
+        Rational u = new Rational(1,2); 
+        t.divide(u);
+        System.out.println(t);
+	Rational v = new Rational(2,3); 
+        Rational w = new Rational(1,2); 
+        v.add(w);
+        System.out.println(v);
     }
 }
